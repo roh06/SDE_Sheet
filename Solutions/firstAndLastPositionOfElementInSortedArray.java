c6bf4e546da46cc0ff2d617cc19b3234d6828304
roh06 <rohansachan08@gmail.com>
2022-06-07 23:55:07 +0530

    Update firstAndLastPositionOfElementInSortedArray.java

--- Solutions/firstAndLastPositionOfElementInSortedArray.java
+++ Solutions/firstAndLastPositionOfElementInSortedArray.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class firstAndLastPositionOfElementInSortedArray {
-    public static int[] solve(int[]nums, int target){
+    public static int[] nonOptimised(int[]nums, int target){
         HashMap<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
@@ -33,7 +33,6 @@ public class firstAndLastPositionOfElementInSortedArray {
         }
         return res;
     }
-
     public static int findFirst(int[] nums, int target) {
         int index=-1;
         for(int i=0;i<nums.length;i++){
@@ -44,11 +43,57 @@ public class firstAndLastPositionOfElementInSortedArray {
         }
         return index;
     }
+    public static int[] solve(int[] nums, int target){
+        int[]arr = new int[2];
+        arr[0] = findFirstIndex(nums,target);
+        arr[1] = findLastIndex(nums,target);
+        return arr;
+    }
+    public static int findFirstIndex(int[]arr, int target){
+        int low = 0;
+        int high = arr.length-1;
+        int fi = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] < target){
+                low = mid+1;
+            }
+            else if(arr[mid] > target){
+                high = mid-1;
+            }
+            else{
+                fi = mid;
+                high = mid-1;
+            }
+        }
+        return fi;
+    }
+    public static int findLastIndex(int[]arr, int target){
+        int low = 0;
+        int high = arr.length-1;
+        int li = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] < target){
+                low = mid+1;
+            }
+            else if(arr[mid] > target){
+                high = mid-1;
+            }
+            else{
+                li = mid;
+                low = mid+1;
+            }
+        }
+        return li;
+    }
+
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[]arr = {5,7,7,8,8,10};
-        int target = 2;
+        int target = 8;
         int[]res = solve(arr,target);
         for(int i : res){
             System.out.print(i+" ");
